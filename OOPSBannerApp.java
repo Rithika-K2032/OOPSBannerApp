992c32c3816f83a184bad883987bc4e62ceb3629
Rithika-K2032 <Rithika1736@gmail.com>
2026-02-23 18:17:45 +0530

    Fourth use case

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,30 +1,80 @@
+import java.util.*;
+
+class BannerPattern {
+
+    // Map to store character patterns
+    private Map<Character, String[]> patternMap = new HashMap<>();
+
+    // Constructor loads patterns
+    public BannerPattern() {
+        patternMap.put('O', new String[]{
+                "******* ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "*     * ",
+                "******* "
+        });
+
+        patternMap.put('P', new String[]{
+                "******  ",
+                "*     * ",
+                "*     * ",
+                "******  ",
+                "*       ",
+                "*       ",
+                "*       "
+        });
+
+        patternMap.put('S', new String[]{
+                " ****** ",
+                "*       ",
+                "*       ",
+                " *****  ",
+                "      * ",
+                "      * ",
+                " *****  "
+        });
+    }
+
+    // Method to generate banner lines
+    public String[] generateBanner(String text) {
+
+        int height = 7;
+        String[] bannerLines = new String[height];
+
+        for (int i = 0; i < height; i++) {
+
+            List<String> rowParts = new ArrayList<>();
+
+            for (char ch : text.toCharArray()) {
+                String[] letter = patternMap.get(ch);
+                rowParts.add(letter[i]);
+            }
+
+            bannerLines[i] = String.join(" ", rowParts);
+        }
+
+        return bannerLines;
+    }
+
+    // Method to print banner
+    public void printBanner(String text) {
+        String[] banner = generateBanner(text);
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
 
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
 
-        // Each line represents one row of the banner
-        // Letters: O   O   P   S
+        BannerPattern banner = new BannerPattern();
 
-        System.out.println(String.join(" ",
-                " *** ", " *** ", " **** ", " **** "));
-
-        System.out.println(String.join(" ",
-                "*   *", "*   *", "*   *", "*     "));
-
-        System.out.println(String.join(" ",
-                "*   *", "*   *", "*   *", "*     "));
-
-        System.out.println(String.join(" ",
-                "*   *", "*   *", "**** ", " *** "));
-
-        System.out.println(String.join(" ",
-                "*   *", "*   *", "*     ", "    *"));
-
-        System.out.println(String.join(" ",
-                "*   *", "*   *", "*     ", "    *"));
-
-        System.out.println(String.join(" ",
-                " *** ", " *** ", "*     ", "**** "));
+        System.out.println("=== OOPS Banner ===\n");
+        banner.printBanner("OOPS");
     }
 }
