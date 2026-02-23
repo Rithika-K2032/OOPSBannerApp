5ea2d4e5007a1c2a74547721ae33ff666f875124
Rithika-K2032 <Rithika1736@gmail.com>
2026-02-23 18:29:45 +0530

    fifth use case

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,80 +1,26 @@
-import java.util.*;
-
-class BannerPattern {
-
-    // Map to store character patterns
-    private Map<Character, String[]> patternMap = new HashMap<>();
-
-    // Constructor loads patterns
-    public BannerPattern() {
-        patternMap.put('O', new String[]{
-                "******* ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "******* "
-        });
-
-        patternMap.put('P', new String[]{
-                "******  ",
-                "*     * ",
-                "*     * ",
-                "******  ",
-                "*       ",
-                "*       ",
-                "*       "
-        });
-
-        patternMap.put('S', new String[]{
-                " ****** ",
-                "*       ",
-                "*       ",
-                " *****  ",
-                "      * ",
-                "      * ",
-                " *****  "
-        });
-    }
-
-    // Method to generate banner lines
-    public String[] generateBanner(String text) {
-
-        int height = 7;
-        String[] bannerLines = new String[height];
-
-        for (int i = 0; i < height; i++) {
-
-            List<String> rowParts = new ArrayList<>();
-
-            for (char ch : text.toCharArray()) {
-                String[] letter = patternMap.get(ch);
-                rowParts.add(letter[i]);
-            }
-
-            bannerLines[i] = String.join(" ", rowParts);
-        }
-
-        return bannerLines;
-    }
-
-    // Method to print banner
-    public void printBanner(String text) {
-        String[] banner = generateBanner(text);
-
-        for (String line : banner) {
-            System.out.println(line);
-        }
-    }
-}
+/**
+ * OOPSBannerApp UC5 – Render OOPS as Banner using Inline Array Initialization
+ * Demonstrates concise array initialization using String.join()
+ */
 
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        BannerPattern banner = new BannerPattern();
+        // Declare and initialize banner lines in one statement
+        String[] lines = {
+            String.join("  ", " *** ", " *** ", "**** ", " ****"),
+            String.join("  ", "*   *", "*   *", "*   *", "*    "),
+            String.join("  ", "*   *", "*   *", "*   *", "*    "),
+            String.join("  ", "*   *", "*   *", "**** ", " *** "),
+            String.join("  ", "*   *", "*   *", "*    ", "    *"),
+            String.join("  ", "*   *", "*   *", "*    ", "    *"),
+            String.join("  ", " *** ", " *** ", "*    ", "**** ")
+        };
 
-        System.out.println("=== OOPS Banner ===\n");
-        banner.printBanner("OOPS");
+        // Print banner
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
