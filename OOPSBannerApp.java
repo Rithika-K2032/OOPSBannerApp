4d96d6f935fc6afe3b23f2bbe0514445185fcc98
Rithika-K2032 <Rithika1736@gmail.com>
2026-03-02 12:58:57 +0530

    Sixth use case

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,50 @@
-/**
- * OOPSBannerApp UC5 – Render OOPS as Banner using Inline Array Initialization
- * Demonstrates concise array initialization using String.join()
- */
-
 public class OOPSBannerApp {
-
-    public static void main(String[] args) {
-
-        // Declare and initialize banner lines in one statement
-        String[] lines = {
-            String.join("  ", " *** ", " *** ", "**** ", " ****"),
-            String.join("  ", "*   *", "*   *", "*   *", "*    "),
-            String.join("  ", "*   *", "*   *", "*   *", "*    "),
-            String.join("  ", "*   *", "*   *", "**** ", " *** "),
-            String.join("  ", "*   *", "*   *", "*    ", "    *"),
-            String.join("  ", "*   *", "*   *", "*    ", "    *"),
-            String.join("  ", " *** ", " *** ", "*    ", "**** ")
+    public static String[] getOPattern() {
+        return new String[] {
+            String.join("", "  *****  "),
+            String.join("", " **   ** "),
+            String.join("", " **   ** "),
+            String.join("", " **   ** "),
+            String.join("", " **   ** "),
+            String.join("", " **   ** "),
+            String.join("", "  *****  ")
         };
-
-        // Print banner
-        for (String line : lines) {
+    }
+    public static String[] getPPattern() {
+        return new String[] {
+            String.join("", " ******  "),
+            String.join("", " **   ** "),
+            String.join("", " **   ** "),
+            String.join("", " ******  "),
+            String.join("", " **       "),
+            String.join("", " **       "),
+            String.join("", " **       ")
+        };
+    }
+    public static String[] getSPattern() {
+        return new String[] {
+            String.join("", "  *****  "),
+            String.join("", " **       "),
+            String.join("", " **       "),
+            String.join("", "  *****  "),
+            String.join("", "      ** "),
+            String.join("", "      ** "),
+            String.join("", " *****   ")
+        };
+    }
+    public static void main(String[] args) {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+        String[] banner = new String[7];
+        for (int i = 0; i < oPattern.length; i++) {
+            banner[i] = String.join("   ",
+                    oPattern[i],
+                    oPattern[i],
+                    pPattern[i],
+                    sPattern[i]);
+        }
+        for (String line : banner) {
             System.out.println(line);
         }
     }
